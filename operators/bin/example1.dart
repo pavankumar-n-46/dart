ae17241fa61aa0fbf8bc41c6168ace9229981f23
pavann-cred <93986467+pavann-cred@users.noreply.github.com>
2023-02-19 07:52:57 +0530

    basic operators

---
+++ operators/bin/example1.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  const age1 = 62;
+  const age2 = 30;
+
+  print(age1 + age2);
+
+  print(age1 - age2);
+
+  const multiply = age1 * age2;
+  print(multiply);
+
+  const divide = age1 / age2;
+  print(divide);
+
+  const intDivide = age1 ~/ age2;
+  print(intDivide);
+}
