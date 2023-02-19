8952e461027679586d654a11c1c46646ae8d9cb5
pavann-cred <93986467+pavann-cred@users.noreply.github.com>
2023-02-19 08:28:28 +0530

    postfix example

---
+++ operators/bin/example3.dart
@@ -0,0 +1,10 @@
+/// Unary postfix operator
+void main(List<String> args) {
+  var age = 30;
+  print(age--); // first it will print age then it will decrease the age
+  print(age);
+  print("\n");
+
+  print(age++);
+  print(age);
+}
